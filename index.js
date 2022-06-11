428c2f3b20022d6a9f9ce7b77d481bc3d14ffe4f
Pavel Silber <silberpav@gmail.com>
2022-06-11 18:09:22 +0300

    start

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// img arr
 let images = document.querySelectorAll('.infinity-slider img')
 let currentSlide = 0
 
@@ -5,32 +6,38 @@ function slider() {
     for (let i = 0; i < images.length; i++) {
         images[i].classList.add('opacity0')
     }
-
-    // document.querySelector('.btn-infinity-up').onclick = function () {
     images[currentSlide].classList.remove('opacity0')
 
-    if (currentSlide + 1 == images.length) {
+    // // Auto play
+    // images[currentSlide].classList.remove('opacity0')
+    // if (currentSlide + 1 == images.length) {
+    //     currentSlide = 0;
+    // } else {
+    //     currentSlide++
+    // }
+}
+
+// // Auto play
+// setInterval(() => {
+//     slider()
+// }, 2000)
+
+
+document.querySelector('.btn-infinity-left').onclick = function () {
+    if (currentSlide == images.length - 1) {
+        // console.log(images.length); // 4
         currentSlide = 0;
     } else {
         currentSlide++
     }
+    slider()
 }
 
-
-
-
-
-setInterval(() => {
+document.querySelector('.btn-infinity-right').onclick = function () {
+    if (currentSlide == 0) {
+        currentSlide = 3;
+    } else {
+        currentSlide--
+    }
     slider()
-}, 2000)
-
-
-// document.querySelector('.btn-infinity-up').onclick = function () {
-//     images[currentSlide].classList.remove('opacity0')
-
-//     if (currentSlide + 1 == images.length) {
-//         currentSlide = 0;
-//     } else {
-//         currentSlide++
-//     }
-// }
+}
